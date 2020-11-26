4e269d7af9fb5e8d767f4d09770b30390e792df3
Eugenuss <eugenedevs@gmail.com>
2020-11-26 11:47:43 +0300

    initial

---
+++ HotelAIS/HotelAIS/Room.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelAIS
+{
+    public class Room
+    {
+        public Room(int id, string status, DateTime rateEndTime)
+        {
+            this.id = id;
+            this.status = status;
+            this.rateEndTime = rateEndTime;
+        }
+
+        public int id { get; set; }
+        public string status { get; set; }
+        public DateTime rateEndTime { get; set; }
+    }
+}
---
+++ HotelAIS/HotelAIS/Windows/RoomsWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelAIS.Windows
+{
+    /// <summary>
+    /// Interaction logic for RoomsWindow.xaml
+    /// </summary>
+    public partial class RoomsWindow : Window
+    {
+        List<Room> rooms = new List<Room>();
+        public RoomsWindow()
+        {
+            InitializeComponent();
+            rooms.Add(new Room(0, "занята", new DateTime(2020, 11, 28)));
+            rooms.Add(new Room(1, "свободна", new DateTime(2020, 11, 28)));
+            rooms.Add(new Room(2, "нужна уборка", new DateTime(2020, 11, 28)));
+            RoomsData.ItemsSource = rooms;
+        }
+
+        private void ReturnButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Owner.Show();
+            this.Close();
+        }
+    }
+}
