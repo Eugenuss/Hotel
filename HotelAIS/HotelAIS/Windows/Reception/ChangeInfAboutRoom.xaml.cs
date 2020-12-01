603581f9740a253892a7c555729d101798d54bf8
saval559362 <valera598@yandex.ru>
2020-12-01 13:59:37 +0300

    Commit

---
+++ HotelAIS/HotelAIS/Windows/Reception/ChangeInfAboutRoom.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace HotelAIS.Windows.Reception
+{
+    public partial class ChangeInfAboutRoom : Window
+    {
+        public ChangeInfAboutRoom()
+        {
+            InitializeComponent();
+        }
+        
+        private void ReturnButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Owner.Show();
+            this.Close();
+        }
+    }
+}
---
+++ HotelAIS/HotelAIS/Windows/Reception/MenuFreeRoom.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace HotelAIS.Windows.Reception
+{
+    public partial class MenuFreeRoom : Window
+    {
+        private List<Room> freeRoom = new List<Room>(); 
+        
+        public MenuFreeRoom()
+        {
+            InitializeComponent();
+        }
+        
+        private void ReturnButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Owner.Show();
+            this.Close();
+        }
+    }
+}
