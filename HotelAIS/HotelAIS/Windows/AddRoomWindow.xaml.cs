41bc1a024f13df6c0743f4312db57b279f969d4d
Eugenuss <eugenedevs@gmail.com>
2020-12-01 12:54:40 +0300

    Added needed forms for administrator

---
+++ HotelAIS/HotelAIS/Windows/AddRoomWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelAIS.Windows
+{
+    /// <summary>
+    /// Interaction logic for AddRoomWindow.xaml
+    /// </summary>
+    public partial class AddRoomWindow : Window
+    {
+        Room newRoom;
+        RoomsWindow ownerWin;
+
+        //parameter ownerWin is temp solution
+        public AddRoomWindow(Room newRoom, RoomsWindow ownerWin)
+        {
+            this.newRoom = newRoom;
+            this.ownerWin = ownerWin;
+            InitializeComponent();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            newRoom.cost = Convert.ToInt32(CostTextBox.Text);
+            newRoom.id = Convert.ToInt32(RoomNumTextBox.Text);
+            newRoom.personCount = Convert.ToInt32(RoomPersonNumTextBox.Text);
+            ownerWin.UpdateTable();
+            this.Close();
+        }
+    }
+}
---
+++ HotelAIS/HotelAIS/XApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelAIS
+{
+    class XApp
+    {
+        public static List<User> users = new List<User>()
+        {
+            new User(0, "admin", "admin", "admin")
+        };
+
+        public static List<Room> rooms = new List<Room>();
+    }
+}
