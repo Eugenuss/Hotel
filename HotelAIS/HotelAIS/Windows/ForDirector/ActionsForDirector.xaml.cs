3f2b828edf70e2634e3510ec818250bd4d2b0fb4
MrTregor <tregoranimation@yandex.ru>
2020-12-01 12:53:39 +0300

    Director in process

---
+++ HotelAIS/HotelAIS/Windows/ForDirector/ActionsForDirector.xaml.cs
@@ -0,0 +1,32 @@
+using System.Windows;
+using System.Windows.Controls;
+using Geared.Wpf.Scrollable;
+
+namespace HotelAIS.Windows.ForDirector
+{
+    public partial class ActionsForDirector : Window
+    {
+        public ActionsForDirector()
+        {
+            InitializeComponent();
+        }
+
+
+        private void StatisticMoneyBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            UserControl statisticMoney = new StatisticMoney();
+            statisticMoney.IsEnabled = false;
+
+        }
+
+        private void StatisticGuestsBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void StatisticFreeRoomBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
