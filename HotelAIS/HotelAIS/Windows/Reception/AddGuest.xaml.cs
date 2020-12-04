b8945fd7003bb4170715a147cfb2cbe5dbdd76e9
saval559362 <valera598@yandex.ru>
2020-12-04 17:04:54 +0300

    Added the following features: adding a guest, changing guest information, displaying information about rooms

--- HotelAIS/HotelAIS/Windows/Reception/AddGuest.xaml.cs
+++ HotelAIS/HotelAIS/Windows/Reception/AddGuest.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Documents;
+using MySql.Data.MySqlClient;
 
 namespace HotelAIS.Windows.Reception
 {
@@ -10,7 +12,6 @@ namespace HotelAIS.Windows.Reception
         public AddGuest()
         {
             InitializeComponent();
-            
         }
 
         private void ReturnBut_Click(object sender, RoutedEventArgs e)
@@ -21,6 +22,20 @@ namespace HotelAIS.Windows.Reception
 
         private void AddGuest_Click(object sender, RoutedEventArgs e)
         {
+            string fname = Convert.ToString(FName.Text);
+            string sname = Convert.ToString(SName.Text);
+            string mname = Convert.ToString(TName.Text);
+            int seria = Convert.ToInt32(Series.Text);
+            int number = Convert.ToInt32(Number.Text);
+            string sql = "INSERT INTO `guests` (FirstName, SecondName, MiddleName, Seria, Nomer) VALUES" + 
+                $" ('{fname}', '{sname}', '{mname}', {seria}, {number});";
+            string connString = "Server=26.146.217.182;Port=3306;Database=hotel;Uid=DoomSlayer;pwd=lilboss;charset=utf8;";
+            MySqlConnection conn = new MySqlConnection(connString);
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.ExecuteNonQuery();
+            conn.Close();
             MessageBox.Show("Гость добавлен!", "Уведомление", MessageBoxButton.OK);
         }
         
--- HotelAIS/HotelAIS/Windows/Reception/ChangeInfAboutRoom.xaml.cs
+++ HotelAIS/HotelAIS/Windows/Reception/ChangeInfAboutRoom.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System.Data;
+using System.Runtime.Remoting.Channels;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using MySql.Data.MySqlClient;
 
 namespace HotelAIS.Windows.Reception
 {
@@ -7,12 +12,29 @@ namespace HotelAIS.Windows.Reception
         public ChangeInfAboutRoom()
         {
             InitializeComponent();
+            string sql = "select * from rooms;";
+            string connString =
+                "Server=26.146.217.182;Port=3306;Database=hotel;Uid=DoomSlayer;pwd=lilboss;charset=utf8;";
+            MySqlConnection connect = new MySqlConnection(connString);
+            connect.Open();
+            MySqlDataAdapter sda = new MySqlDataAdapter(sql, connect);
+            DataTable table = new DataTable();
+            sda.Fill(table);
+            connect.Close();
+            ChangeInfData.ItemsSource = table.DefaultView;
+            
         }
         
         private void ReturnButton_Click(object sender, RoutedEventArgs e)
         {
             this.Owner.Show();
             this.Close();
+        }
+
+        private void DoubleClick_Click(object sender, MouseButtonEventArgs e)
+        {
+            //TO DO
+            //Get selected item and change them
         }
     }
 }
