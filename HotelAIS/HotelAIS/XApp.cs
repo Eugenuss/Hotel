844bdd7c7d6f8f85598c2c945593b8a5afbd26fb
MrTregor <tregoranimation@yandex.ru>
2020-12-18 14:47:31 +0300

    Finish Again

--- HotelAIS/HotelAIS/XApp.cs
+++ HotelAIS/HotelAIS/XApp.cs
@@ -14,9 +14,7 @@ namespace HotelAIS
 
         public static void openDBConnection()
         {
-            string connString =
-                // "Server=localhost;Port=3306;Database=hotel;Uid=mysql;pwd=mysql;charset=utf8;";
-                "Server=26.146.217.182;Port=3306;Database=hotel;Uid=DoomSlayer;pwd=lilboss;charset=utf8;";
+            string connString = MySqlConnect.connString;
             connection = new MySqlConnection(connString);
             connection.Open();
         }
